cfe9a5f95abe284179b7ef4ff5c18f65de35ef4b
M00n-man92 <azraelnumb00@gmail.com>
2021-12-24 05:31:49 +0300

    starboy

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import datetime
+onlyyoufeelmybody=datetime.now()
+print(onlyyoufeelmybody)
+http="https://pixe.la/v1/users"
+bythat="realsuccessintheindustry"
+attheendoftheday="askdlfjas32434"
+token="thjisibtthettoken9023@#@34SFsd"
+param={"token":"thjisibtthettoken9023@#@34SFsd",
+"username":"realsuccessintheindustry",
+"agreeTermsOfService":"yes",
+"notMinor":"yes"}
+everybodyaskingome={"X-USER-TOKEN":token}
+mybabybadbadbado={"id":attheendoftheday,
+"name":"starboy",
+"unit":"page",
+"type":"int",
+"color":"momiji",
+
+}
+shegotherownbutsheneedsomelove={"date":"20211224",
+"quantity":"3"}
+# mindinmyown=requests.post(url=http,json=param)
+
+ofcredit=f"{http}/{bythat}/graphs"
+janged=f"{http}/{bythat}/graphs"
+unconditionalpro=f"{http}/{bythat}/graphs/{attheendoftheday}"
+dontlie=requests.get(url=janged,headers=everybodyaskingome)
+babygirl=requests.post(url=unconditionalpro,headers=everybodyaskingome,json=shegotherownbutsheneedsomelove)
+# platonmybango=requests.post(url=ofcredit,headers=everybodyaskingome,json=mybabybadbadbado)
+print(babygirl.json())
